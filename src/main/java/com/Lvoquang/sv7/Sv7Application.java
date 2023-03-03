c5b31e636964578337943c6928c2aa7a4807520c
lvoquang <voquangluu997@gmail.com>
2023-03-03 15:37:39 +0700

    init project

---
+++ src/main/java/com/Lvoquang/sv7/Sv7Application.java
@@ -0,0 +1,13 @@
+package com.Lvoquang.sv7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sv7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sv7Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Lvoquang/sv7/Sv7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Lvoquang.sv7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sv7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
